14c14534f2146ae881e6a01cfc2ace4769551c43
Chris Ryan <creditdesign@gmail.com>
2017-11-02 12:48:25 +0000

    Colours featured genes black

--- assets/js/goomba/svg/gene-build.js
+++ assets/js/goomba/svg/gene-build.js
@@ -18,7 +18,19 @@ function buildGenes() {
 			.attr("width", 0)
 			.attr("stroke", "none")
 			.attr("stroke-width", 0)
-			.attr("fill", "#CE1421")
+			.attr("fill", d => {
+				if (d.symbol === "CD4" || 
+					d.symbol === "TP53"  ||
+					d.symbol === "GRB2" ||
+					d.symbol === "HBB" ||
+					d.symbol === "TNF" ||
+					d.symbol === "APOE") {
+					console.log(d);
+					return "#000000;"
+				} else {
+					return "#CE1421"
+				}
+			})
 			.transition(that.duration)
 			.delay( (d,i) => {
 				return (that.duration * 1.8) + (that.yt(d.start) * 0.01 * that.delay);
--- build/js/main.js
+++ build/js/main.js
@@ -6909,7 +6909,14 @@ function buildGenes() {
 		// Enter
 		d3.select(this).selectAll("rect").data(data.genes).enter().append("rect").attr("class", "gene contracted").attr("y", function (d) {
 			return that.yt(d.start);
-		}).attr("x", 0).attr("height", 2).attr("width", 0).attr("stroke", "none").attr("stroke-width", 0).attr("fill", "#CE1421").transition(that.duration).delay(function (d, i) {
+		}).attr("x", 0).attr("height", 2).attr("width", 0).attr("stroke", "none").attr("stroke-width", 0).attr("fill", function (d) {
+			if (d.symbol === "CD4" || d.symbol === "TP53" || d.symbol === "GRB2" || d.symbol === "HBB" || d.symbol === "TNF" || d.symbol === "APOE") {
+				console.log(d);
+				return "#000000;";
+			} else {
+				return "#CE1421";
+			}
+		}).transition(that.duration).delay(function (d, i) {
 			return that.duration * 1.8 + that.yt(d.start) * 0.01 * that.delay;
 		}).duration(that.duration).attr("width", function (d) {
 			return that.geneScale(parseInt(d.count, 10));
